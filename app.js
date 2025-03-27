9a06c6c29560d28c3eb0166a6e7375394d2fd2f0
scruffyjadd <117733480+scruffyjadd@users.noreply.github.com>
2025-03-27 06:34:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const examsRouter = require('./routes/exams');
 
+const app = express();
+const port = 3000;
+
+app.use.(express.json());
+
+app.get('/exam-group', (req, res) => {
+  res.json({ message: 'Group <your_group> API' });
+});
+
+app.use('/exams', examsRouter);
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
